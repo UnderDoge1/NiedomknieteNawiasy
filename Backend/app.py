3fc099574400a5d615e0bc829baea987539c9f5b
UnderDoge1 <f.p.dudek@gmail.com>
2024-12-07 17:14:05 +0100

    add basic endpoints

--- Backend/app.py
+++ Backend/app.py
@@ -1,1 +1,18 @@
-#elosmiecie
+from flask import Flask, jsonify
+app = Flask(__name__)
+
+@app.route('/')
+def start():
+    return 'sranie -- endpoint post,get ----> /register'
+@app.route('/resiter', methods=['POST'])
+def register_spaceship():
+    return jsonify({'message': 'sranie'}), 501
+
+@app.route('/register', methods=['GET'])
+def get_spaceships_register():
+    return 'niezesrajsie'
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
